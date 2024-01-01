9dcc2eef8e83156d96bfa74a98c2a299314ece04
Artem Vlasenko <s18b3_vlasenko@179.ru>
2024-01-01 18:18:12 +0300

    try to animate favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
@@ -9,6 +9,30 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-app';
+
+  ngOnInit(): void {
+    let counter = 0;
+    let qua = 4;
+    let nodeFavicon = document.querySelectorAll("link[rel='icon']");
+    let timer: NodeJS.Timeout | null = null;
+
+    function animateFavicon() {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      for (let i = 0; i < nodeFavicon.length; i++) {
+        nodeFavicon[i].setAttribute("href", `./assets/icons/outsidious_${counter}.svg`);
+      }
+      counter = (counter + 1) % qua;
+      //timer = setTimeout(animateFavicon, 5000);
+    }
+
+    //setTimeout(animateFavicon, 5000);
+
+    document.addEventListener('click', () => {
+      animateFavicon();
+    })
+  }
 }
