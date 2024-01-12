cb9db3071949746b1f3fa20a1ad5007d7c5cd78f
Artem Vlasenko <s18b3_vlasenko@179.ru>
2024-01-12 12:42:59 +0300

    add theme toggle and logo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { TuiRootModule } from "@taiga-ui/core";
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAngularSvgIcon } from "angular-svg-icon";
+import { HttpClientModule } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideRouter(appRoutes), importProvidersFrom(TuiRootModule)],
+  providers: [provideAnimations(), provideRouter(appRoutes), importProvidersFrom(TuiRootModule), importProvidersFrom(HttpClientModule), provideAngularSvgIcon()],
 };
